fed2ec2c2d8648bc3505af7888b9939d8e121e2a
Srikant V S <srikant.vs@softlandindia.co.in>
2025-03-17 14:25:03 +0530

    Initial commit

---
+++ vendors/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('manage-order/', views.manage_order, name='manage-order'),
+    path('api/update-order/', views.update_order, name='update-order'),
+    path('api/get-current-time/', views.get_current_time, name='get-current-time'),
+    path('api/list-order/', views.list_order, name='list-order'),
+    # Add the proxy endpoint
+    path('proxy/api/update-order/', views.proxy_update_order, name='proxy-update-order'),
+    path('api/save-subscription/', views.save_subscription, name='save-subscription'),
+    
+]
